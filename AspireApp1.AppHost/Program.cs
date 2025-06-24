9c5a4b841fa3a8ac5f3af82dcd58806ca7f65322
sataphon <ataphon.sri@hitachi-ch.co.th>
2025-06-24 14:58:46 +0700

    Add project files.

---
+++ AspireApp1.AppHost/Program.cs
@@ -0,0 +1,33 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+builder.AddDockerComposeEnvironment("compose");
+
+var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
+
+var frontend = builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.AddYarp("apiGateWay")
+    .WithConfigFile("yarp.json")
+    .WithReference(apiService)
+    .WithReference(frontend)
+    //.WithContainerFiles("/etc", new ContainerFile[]
+    //{
+    //    new ContainerFile()
+    //    {
+    //        Contents = File.ReadAllText("yarp.json"),
+    //        Name = "yarp.config",
+    //        //Mode = UnixFileMode.UserExecute | UnixFileMode.UserRead | UnixFileMode.UserWrite | UnixFileMode.GroupRead | UnixFileMode.GroupWrite,
+    //        Mode = UnixFileMode.UserRead | UnixFileMode.OtherRead,
+    //    },
+    //},
+    //defaultOwner: 1000)
+    .WithExternalHttpEndpoints();
+
+builder.Build().Run();
